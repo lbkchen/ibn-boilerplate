f8e0ea3d09ec255ee594e5e9edc21151a4e99439
tonyanguyen <tonyanguyen@berkeley.edu>
2019-03-15 23:37:55 -0700

    o k a y

--- src/components/FAQPage.jsx
+++ src/components/FAQPage.jsx
@@ -15,7 +15,7 @@ class FAQPage extends React.Component {
       <div className="container">
        
        <div className="FAQ-page-landing">
-       <h1>Frequently Asked Questions</h1>
+       <h1 className="berkeley-blue">Frequently Asked Questions</h1>
        <p>
        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Don’t see what you need? Submit a queston here.
        </p>
