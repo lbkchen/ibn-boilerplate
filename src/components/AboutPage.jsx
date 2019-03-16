a41908fe1b87c516e7bae94d4312f4d9cfbefa70
tonyanguyen <tonyanguyen@berkeley.edu>
2019-03-15 23:24:30 -0700

    Static Pages. Need to refine css, likely will change most of them into components.

--- src/components/AboutPage.jsx
+++ src/components/AboutPage.jsx
@@ -16,6 +16,7 @@ import karina from "../images/About_13_Karina_Candela.jpg";
 import about_food_supp  from "../images/About_14_Food Support.jpg";
 import about_housing from "../images/About_15_Housing_Support.jpg";
 import about_econ from "../images/About_16_Economic_Support.jpg";
+import maria from "../images/About_17_Maria.png";
 import about_faq from "../images/About_17_FAQ.jpg";
 
 class AboutPage extends React.Component {
@@ -72,36 +73,37 @@ class AboutPage extends React.Component {
           </div>
 
           {/* BNS Background  */}
-          <div className="about-page-background ">
-            <div className="about-page-background-text">
-              <h1>Background</h1>
-            </div>
-            <div className="right"><img className="about-img" src={about_background}></img></div>
-            <div className="about-page-background-text">
-              <p>
-                Basic Needs Security refers to the food, housing, and economic security of our community. We understand that
-                basic needs have a direct impact on the mental-emotional-physical health, wellness, academic performance,
-                professional development, and holistic success of students.
-              </p>
-              <p>
-                The quality of life of our students has a major impact on their sense of belonging, persistence, graduation,
-                and overall experience. Therefore, we refuse to accept hunger, malnourishment, and homelessness as part of
-                our university.
-              </p>
-              <p>Basic Needs is committed to accomplishing food, housing and economic justice on the UC Berkeley campus
-                through a robust model of prevention, intervention and emergency relief efforts. Our goal is to ensure that
-                UC Berkeley is a Basic Needs Secure campus. Our prevention efforts thus far have included pre-college
-                education, first year student outreach and messaging around Basic Needs, as well as Basic Needs skill
-                development such as NST 20 lecture, and nutrition and cooking demonstration. Emergency interventions include
-                the UC Berkeley Food Pantry which provides emergency food relief. Crisis interventions include the Food
-                Assistance Program which provides eligible students with financial support for food, as well as the Emergency
-                Housing Support program.</p>
-
-              
-            </div>
+          <div className="about-page-background">
+          <div className="background-block">
+          <div className="left">
             
+            <div className="about-page-background-text">
+            <h1>Our Background</h1>
+              <p>
+              Basic Needs Security refers to the food, housing, and economic security of our community. We understand that basic needs have a direct impact on the mental-emotional-physical health, wellness, academic performance, professional development, and holistic success of students.
+              </p>
+              <p>The quality of life of our students has a major impact on their sense of belonging, persistence, graduation, and overall experience. Therefore, we refuse to accept hunger, malnourishment, and homelessness as part of our university.</p>
+              <p>Basic Needs is committed to accomplishing food, housing and economic justice on the UC Berkeley campus through a robust model of prevention, intervention and emergency relief efforts. Our goal is to ensure that UC Berkeley is a Basic Needs Secure campus. Our prevention efforts thus far have included pre-college education, first year student outreach and messaging around Basic Needs, as well as Basic Needs skill development such as NST 20 lecture, and nutrition and cooking demonstration. Emergency interventions include the UC Berkeley Food Pantry which provides emergency food relief. Crisis interventions include the Food Assistance Program which provides eligible students with financial support for food, as well as the Emergency Housing Support program.</p>
+
+            
+          
+
+            </div>
+        </div>
+
+        <div className="right">
+         <img className="about-img" src={about_background}></img>
+        </div>
+          
+          
+          </div>
+
+          
             
           </div>
+            
+            
+         
           {/* Our Team  */}
           <div className="about-page-team ">
             <h1>Our Basic Needs Village</h1>
@@ -215,7 +217,7 @@ class AboutPage extends React.Component {
                 {/* column */}
                 <div className="about-page-team-column">
                   <div className="about-page-team-card">
-                    <img className="about-page-team-img" src={sara} alt="Img"  />
+                    <img className="about-page-team-img" src={maria} alt="Img"  />
                     <div className="about-page-team-text-container">
                       <h3>Maria Balcazar Tellez</h3>
                       <p className="about-page-team-description">Graduate Student Researcher
@@ -256,7 +258,7 @@ class AboutPage extends React.Component {
                 {/* column */}
      
                   <div className="about-page-focus-card">
-                  <div className="left-img"><img className="about-focus-img" src={about_food_supp}></img></div>
+                  <div className="focus-img"><img className="about-focus-img" src={about_food_supp}></img></div>
                     <div className="about-page-focus-text-container">
                       <h3>Food Support
                       </h3>
@@ -277,7 +279,7 @@ class AboutPage extends React.Component {
                   </div>
         
                   <div className="about-page-focus-card">
-                  <img className="about-focus-img" src={about_housing}></img>
+                  <div className="focus-img"><img className="about-focus-img" src={about_housing}></img></div>
                     <div className="about-page-focus-text-container">
                       <h3>Housing Support</h3>
                       <br/>
@@ -299,7 +301,7 @@ class AboutPage extends React.Component {
              
       
                   <div className="about-page-focus-card">
-                  <img className="about-focus-img" src={about_econ}></img>
+                  <div className="focus-img"><img className="about-focus-img" src={about_econ}></img></div>
                     <div className="about-page-focus-text-container">
                       <h3>Economic Support</h3>
                       <br/>
--- src/components/LearnPage.jsx
+++ src/components/LearnPage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Button, Intent } from "@blueprintjs/core";
 import { Link } from "react-router-dom";
-
+import research_icon from "../images/research-icon.png";
 class LearnPage extends React.Component {
   render() {
     return (
@@ -55,40 +55,192 @@ class LearnPage extends React.Component {
               <table className="learn-page-text">
                 <tbody><tr>
                     <th>Name</th>
-                    <th>Date</th>
+                    {/* <th>Date</th> */}
                   </tr>
                   <tr>
-                    <td>UC Berkeley Chancellor’s Housing Survey
+                    <td>
+                    <img className="research-icon" src={research_icon}>
+                    </img>
+                      UC Berkeley Chancellor’s Housing Survey
                     </td>
-                    <td>Feb 22</td>
+            
                   </tr>
                   <tr>
-                    <td>UC Berkeley Total Cost of Attendance Report
+                    <td><img className="research-icon" src={research_icon}>
+                    </img>UC Berkeley Total Cost of Attendance Report
                     </td>
-                    <td>Feb 22</td>
+            
                   </tr>
                   <tr>
-                    <td>UC Berkeley Housing Survey 2017 Preliminary Findings
+                    <td><img className="research-icon" src={research_icon}>
+                    </img>UC Berkeley Housing Survey 2017 Preliminary Findings
                     </td>
-                    <td>Feb 22</td>
+            
                   </tr>
                   <tr>
-                    <td>UC Info Center (Undergraduate alumni at work)
+                    <td><img className="research-icon" src={research_icon}>
+                    </img>UC Info Center (Undergraduate alumni at work)
                     </td>
-                    <td>Feb 22</td>
+            
                   </tr>
                 </tbody></table>
               <br />
+
+              <h4>Statewide Research</h4>
+              <br />
+              <table className="learn-page-text">
+                <tbody><tr>
+                    <th>Name</th>
+                    {/* <th>Date</th> */}
+                  </tr>
+                  <tr>
+                    <td> <img className="research-icon" src={research_icon}>
+                    </img>Student Food Access and Security Study
+                    </td>
+            
+                  </tr>
+                  <tr>
+                    <td> 
+                    <img className="research-icon" src={research_icon}>
+                    </img>No Food for Thought
+                    </td>
+            
+                  </tr>
+                </tbody></table>
+              <br />
+
+              <h4>National Research</h4>
+              <br />
+              <table className="learn-page-text">
+                <tbody><tr>
+                    <th>Name</th>
+                    {/* <th>Date</th> */}
+                  </tr>
+                  <tr>
+                    <td><img className="research-icon" src={research_icon}>
+                    </img>Food and Housing Insecurity in College
+
+                    </td>
+            
+                  </tr>
+                  <tr>
+                    <td><img className="research-icon" src={research_icon}>
+                    </img>Prevalence and Correlates of Food Insecurity Among Students Attending a Midsize Rural University in Oregon
+                    </td>
+            
+                  </tr>
+                  <tr>
+                    <td><img className="research-icon" src={research_icon}>
+                    </img>"Factors Related to the High Rates of Food Insecurity among Diverse, Urban College Freshmen"
+
+                    </td>
+            
+                  </tr>
+                  <tr>
+                    <td><img className="research-icon" src={research_icon}>
+                    </img>Food Insecurity at CUNY: Results from a Survey of CUNY Undergraduate Students
+                    </td>
+            
+                  </tr>
+
+
+                  <tr>
+                    <td><img className="research-icon" src={research_icon}>
+                    </img>Development, Evaluation, and Validation of Environmental Assessment Tools to Evaluate the College Nutrition Environment
+
+                    </td>
+            
+                  </tr>
+                  <tr>
+                    <td><img className="research-icon" src={research_icon}>
+                    </img>Food insecurity prevalence among college students at the University of Hawai’i at Manoa
+                    </td>
+            
+                  </tr>
+                  <tr>
+                    <td><img className="research-icon" src={research_icon}>
+                    </img>Student food insecurity: The skeleton in the university closet
+                    </td>
+            
+                  </tr>
+                  <tr>
+                    <td><img className="research-icon" src={research_icon}>
+                    </img>Food insecurity: is it an issue among tertiary students?
+
+                    </td>
+            
+                  </tr>
+                  <tr>
+                    <td><img className="research-icon" src={research_icon}>
+                    </img>Food insecurity among university students in Victoria: A pilot study
+
+                    </td>
+            
+                  </tr>
+                  <tr>
+                    <td><img className="research-icon" src={research_icon}>
+                    </img>Children Are Aware of Food Insecurity and Take Responsibility for Managing Food Resources
+                    </td>
+            
+                  </tr>
+                  <tr>
+                    <td><img className="research-icon" src={research_icon}>
+                    </img>Nine Charts about Wealth Inequality in America
+
+                    </td>
+            
+                  </tr>
+                  <tr>
+                    <td><img className="research-icon" src={research_icon}>
+                    </img>Bridging the Racial Generation Gap Is Key to America's Economic Future
+
+                    </td>
+            
+                  </tr>
+                  <tr>
+                    <td><img className="research-icon" src={research_icon}>
+                    </img>Interactive Map: SNAP Rose In States to Meet Needs but Participation Has Fallen as Economy Recovered
+                    </td>
+            
+                  </tr>
+                  <tr>
+                    <td><img className="research-icon" src={research_icon}>
+                    </img>The Eviction Lab
+
+                    </td>
+            
+                  </tr>
+                  <tr>
+                    <td><img className="research-icon" src={research_icon}>
+                    </img>National Center for Education Statistics (Earning by education level)
+
+                    </td>
+            
+                  </tr>
+                
+                </tbody></table>
+              <br />
+
             </div>
             <div id="learn-page-multimedia">
               <h2>Multimedia</h2>
-              <p>Read and hear more about basic needs in the news!
-              </p>
+              {/* <p>Read and hear more about basic needs in the news!
+              </p> */}
+
+              <iframe width="100%" height="290" src="https://www.npr.org/player/embed/513902272/514731729" frameborder="0"></iframe>
+
+              <iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/297579027&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
+              
             </div>
             <div id="learn-page-news">
               <h2>Basic Needs in the News</h2>
-              <p>Read and hear more about basic needs in the news!
-              </p>
+              {/* <p>Read and hear more about basic needs in the news!
+              </p> */}
+              <iframe width="560" height="315" src="https://www.youtube.com/embed/S6_fMj8VRSk" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+            
+
+              
             </div>
           </div>
         </div>
